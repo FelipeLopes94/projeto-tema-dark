de7802b585ab862f47926c672a614502519a7df2
felipelopes94 <felipee.c.lopess@gmail.com>
2021-04-21 19:23:28 -0300

    Polished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,7 +15,7 @@ const Header: React.FC  <Props> = ({toggleTheme}) => {
     
     return(
         <Container>
-            Hello World
+            Projeto de troca de temas  DARK / LIGHT
 
             <Switch
                 onChange={toggleTheme}
